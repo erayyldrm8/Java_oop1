5826ed0ac40528be9de6de69fc87324aac10280e
ERAY <ERAY@DESKTOP-9JI43N4>
2022-09-26 17:10:18 +0300

    first commit

---
+++ oop1/src/oop1/Main.java
@@ -0,0 +1,14 @@
+package oop1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+
+	}
+
+}
---
+++ oop1/src/oop1/Product.java
@@ -0,0 +1,13 @@
+package oop1;
+// classlar PascalCase isimlendirilir(Product)
+public class Product {
+	
+	//Class ın fieldları camelCase yazılır
+	
+	String name;
+	double unitPrice;
+	double discount;
+	String[] imageUrls;
+	int unitsInStock;
+
+}
