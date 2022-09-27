cfd6e580dbdf9563fcf9e7fc96b86276c08bc635
ERAY <ERAY@DESKTOP-9JI43N4>
2022-09-27 16:43:43 +0300

    .

--- oop1/src/oop1/Main.java
+++ oop1/src/oop1/Main.java
@@ -8,7 +8,63 @@ public class Main {
 		
 		
 		
+		Product product1 = new Product();
+		/*product1.name = "Delonghi Kahve Makinesi"; // set value
+		product1.unitPrice = 7500;
+		product1.discount = 7;
+		product1.unitsInStock = 3;
+		product1.imageUrl = "photo1.jpg";*/
+		
+		product1.setName("Delonghi Kahve Makinesi");
+		product1.setUnitPrice(7500);
+		product1.setDiscount(7);
+		product1.setUnitsInStock(3);
+		product1.setImageUrl("photo1.jpg");
 
+		// System.out.println(product1.name); //get
+
+		Product product2 = new Product();
+		/*product2.name = "Smeg Kahve Makinesi";
+		product2.unitPrice = 6500;
+		product2.discount = 8;
+		product2.unitsInStock = 2;
+		product2.imageUrl = "photo2.jpg";*/
+		
+		product2.setName("Smeg Kahve Makinesi");
+		product2.setUnitPrice(6500);
+		product2.setDiscount(8);
+		product2.setUnitsInStock(2);
+		product2.setImageUrl("photo2.jpg");
+
+		Product product3 = new Product();
+		/*product3.name = "Kitchen Aid Kahve Makinesi";
+		product3.unitPrice = 4500;
+		product3.discount = 9;
+		product3.unitsInStock = 4;
+		product3.imageUrl = "photo3.jpg";*/
+		
+		product3.setName("Kitchen Aid Kahve Makinesi");
+		product3.setUnitPrice(4500);
+		product3.setDiscount(9);
+		product3.setUnitsInStock(4);
+		product3.setImageUrl("photo3.jpg");
+		
+		//Product[] products=new Product[] {product1,product2,product3};
+		
+		/*
+		Product[] products=new Product[3];  
+		products[1]=product1; 
+		products[2]=product2;
+		products[3]=product3;
+		*/
+		
+		Product[] products={product1,product2,product3};
+		
+		System.out.println("<ul>");//unordered list   HTML
+		for(Product productLoop:products) {
+			System.out.println("<li>" + productLoop.getName() + "</li>");//list item
+		}
+		System.out.println("</ul>");
 	}
 
 }
--- oop1/src/oop1/Product.java
+++ oop1/src/oop1/Product.java
@@ -1,13 +1,55 @@
 package oop1;
+
 // classlar PascalCase isimlendirilir(Product)
 public class Product {
-	
-	//Class ın fieldları(alanları) camelCase yazılır
-	
-	String name;
-	double unitPrice;
-	double discount;
-	String imageUrl;
-	int unitsInStock;
+
+	// Class ın fieldları(alanları) camelCase yazılır
+
+	private String name;
+	private double unitPrice;
+	private double discount;
+	private String imageUrl;
+	private int unitsInStock;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public double getDiscount() {
+		//discount=discount+5;   sepette %5 indirim daha
+		return discount;
+	}
+
+	public void setDiscount(double discount) { //bunu silersen bir daha atanamaz
+		this.discount = discount;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public int getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(int unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
 
 }
