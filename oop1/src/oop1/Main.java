e243cc6a57dae5f235682a97a5ce6544c6fffdf2
ERAY <ERAY@DESKTOP-9JI43N4>
2022-09-27 17:13:59 +0300

    .

--- oop1/src/oop1/Main.java
+++ oop1/src/oop1/Main.java
@@ -20,9 +20,10 @@ public class Main {
 		product1.setDiscount(7);
 		product1.setUnitsInStock(3);
 		product1.setImageUrl("photo1.jpg");
-
+		
 		// System.out.println(product1.name); //get
-
+		
+		//------------------------------------------------------------------------
 		Product product2 = new Product();
 		/*product2.name = "Smeg Kahve Makinesi";
 		product2.unitPrice = 6500;
@@ -35,7 +36,7 @@ public class Main {
 		product2.setDiscount(8);
 		product2.setUnitsInStock(2);
 		product2.setImageUrl("photo2.jpg");
-
+		//------------------------------------------------------------------------
 		Product product3 = new Product();
 		/*product3.name = "Kitchen Aid Kahve Makinesi";
 		product3.unitPrice = 4500;
@@ -48,7 +49,7 @@ public class Main {
 		product3.setDiscount(9);
 		product3.setUnitsInStock(4);
 		product3.setImageUrl("photo3.jpg");
-		
+		//------------------------------------------------------------------------
 		//Product[] products=new Product[] {product1,product2,product3};
 		
 		/*
@@ -62,7 +63,8 @@ public class Main {
 		
 		System.out.println("<ul>");//unordered list   HTML
 		for(Product productLoop:products) {
-			System.out.println("<li>" + productLoop.getName() + "</li>");//list item
+			//System.out.println("<li>" + productLoop.name + "</li>");//list item 
+			System.out.println("<li>" + productLoop.getName() + "</li>");//list item, set/get yapınca
 		}
 		System.out.println("</ul>");
 	}
